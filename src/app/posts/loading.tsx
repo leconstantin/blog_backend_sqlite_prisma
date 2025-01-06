fac33ede725bea021aae6f857aeccd12d61dcc88
leconstantin <lcon69184@gmail.com>
2025-01-06 19:00:25 +0200

    Added loadingPosts

--- src/app/posts/loading.tsx
+++ src/app/posts/loading.tsx
@@ -1,13 +1,11 @@
-import Link from "next/link";
-
 export default function Loading() {
   return (
-    <main className="text-center pt-20 px-5 flex flex-col items-center justify-center min-h-52 ">
+    <main className="text-center pt-9 px-5 flex flex-col items-center justify-center min-h-52 ">
       {/* <h1 className="text-4xl font-bold mb-5 block h-5 w-32 bg-zinc-500 rounded-xl animate-pulse"></h1> */}
       <ul>
         {Array.from({ length: 5 }).map((_, i) => (
-          <li key={i} className="mb-3">
-            <p className="block h-2 w-24 bg-zinc-300 animate-pulse rounded-xl"></p>
+          <li key={i} className="mb-6">
+            <p className="block h-2 w-44 bg-zinc-300 animate-pulse rounded-xl"></p>
           </li>
         ))}
       </ul>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -11,6 +11,10 @@ const navLinks = [
     href: "/posts",
     label: "Posts",
   },
+  {
+    href: "/create-post",
+    label: "Create Post",
+  },
 ];
 export default function Header() {
   const pathName = usePathname();
