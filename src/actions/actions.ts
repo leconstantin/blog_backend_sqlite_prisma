57e6687a3a7f54d8a924873054f6eb958d0f798d
leconstantin <lcon69184@gmail.com>
2025-01-06 20:41:27 +0200

    added revalidate

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { prisma } from "@/lib/db";
+import { revalidatePath } from "next/cache";
 
 export async function createPost(formData: FormData) {
   const title = formData.get("title") as string;
@@ -13,4 +14,7 @@ export async function createPost(formData: FormData) {
       body,
     },
   });
+  // revalidate
+
+  revalidatePath("/posts");
 }
